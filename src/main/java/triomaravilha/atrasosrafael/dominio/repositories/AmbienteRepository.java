0cd75cb24fe8f913f712b34c1844aea894a46bec
Kelly <kellydealmeidasantos237@gmail.com>
2026-08-27 09:09:03 -0300

    Entidade "professor" finalizada

--- src/main/java/triomaravilha/atrasosrafael/dominio/repositories/AmbienteRepository.java
+++ src/main/java/triomaravilha/atrasosrafael/dominio/repositories/AmbienteRepository.java
@@ -1,7 +1,7 @@
 package triomaravilha.atrasosrafael.dominio.repositories;
 
 import triomaravilha.atrasosrafael.dominio.Ambiente;
-import triomaravilha.atrasosrafael.infrastructure.entity.AmbienteEntity;
+import triomaravilha.atrasosrafael.infrastructure01.entity.AmbienteEntity;
 
 import java.util.List;
 
--- src/main/java/triomaravilha/atrasosrafael/dominio/repositories/ProfessorRepository.java
+++ src/main/java/triomaravilha/atrasosrafael/dominio/repositories/ProfessorRepository.java
@@ -1,7 +1,7 @@
 package triomaravilha.atrasosrafael.dominio.repositories;
 
 import triomaravilha.atrasosrafael.dominio.Professor;
-import triomaravilha.atrasosrafael.infrastructure.entity.ProfessorEntity;
+import triomaravilha.atrasosrafael.infrastructure01.entity.ProfessorEntity;
 
 import java.util.List;
 
